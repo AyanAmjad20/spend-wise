a7a4b9b0379ecbe31b97b93795fcfe5e7de81a1d
Ayan Amjad <150398687+AyanAmjad20@users.noreply.github.com>
2025-09-24 00:56:37 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
-import { HashRouter } from "react-router-dom";
-root.render(
-  <HashRouter>
-    <App />
-  </HashRouter>
-);
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+
+createRoot(document.getElementById("root")!).render(<App />);
